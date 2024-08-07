f99e2d0634c5d58314499dfc22886beae5bde44e
thisissabhariesh <117589228+thisissabhariesh@users.noreply.github.com>
2024-08-07 15:15:28 +0530

    Delete src directory

--- src/main/java/com/example/demo/repository/ContactRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.ContactModel;
-
-public interface ContactRepository extends JpaRepository<ContactModel, Long> {
-}
